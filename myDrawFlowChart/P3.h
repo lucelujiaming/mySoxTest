05afd7bf351d1dec95d49540ee500aa6c310d069
xungao <xungao@xungao.com>
2023-06-25 11:53:25 +0800

    Update P3 and Transform3

--- myDrawFlowChart/P3.h
+++ myDrawFlowChart/P3.h
@@ -9,17 +9,26 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
-class CP3
+#include "P2.h"
+
+class CP3:public CP2
 {
 public:
 	CP3(void);
-	~CP3(void);
-	CP3(double x,double y,double z);
+	CP3(double x, double y, double z);
+	virtual ~CP3(void);
+	friend CP3 operator + (const CP3 &p0, const CP3 &p1);//运算符重载
+	friend CP3 operator - (const CP3 &p0, const CP3 &p1);
+	friend CP3 operator * (const CP3 &p, double scalar);
+	friend CP3 operator * (double scalar, const CP3 &p);
+	friend CP3 operator / (const CP3 &p, double scalar);
+	friend CP3 operator += (CP3 &p0, CP3 &p1);
+	friend CP3 operator -= (CP3 &p0, CP3 &p1);
+	friend CP3 operator *= (CP3 &p, double scalar);
+	friend CP3 operator /= (CP3 &p, double scalar);
+	double Magnitude(void);//长度
 public:
-	double x;
-	double y;
 	double z;
-    double w;
 };
 
 #endif // !defined(AFX_CP3_H__83A9A336_3155_4360_ACD5_39ABFFB9442A__INCLUDED_)

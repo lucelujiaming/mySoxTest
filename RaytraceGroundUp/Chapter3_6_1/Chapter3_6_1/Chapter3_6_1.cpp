32d79c40910100fb68dd71accd40bb3ecc7c1ba6
xungao <xungao@xungao.com>
2023-09-11 14:16:56 +0800

    Add Chapter3_6_1 VisualStudio2103 project

---
+++ RaytraceGroundUp/Chapter3_6_1/Chapter3_6_1/Chapter3_6_1.cpp
@@ -0,0 +1,24 @@
+// Chapter3_6_1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+#include "World.h"
+
+#include <iostream>
+using namespace std;
+
+
+int
+main(void)
+{
+	cout << "P3\n"
+		<< 400 << " " << 400 << "\n255\n";
+
+	World w;
+	w.build();
+	w.render_scene();
+	return(0);
+}
+
---
+++ RaytraceGroundUp/Chapter3_6_1/Chapter3_6_1/Ray.h
@@ -0,0 +1,27 @@
+#ifndef __RAY__
+#define __RAY__
+
+#include "Point3D.h"
+#include "Vector3D.h"
+
+class Ray {
+
+	public:
+
+		Point3D			o;  	// origin
+		Vector3D		d; 		// direction
+		int				depth;  // ray depth, for reflections
+
+		Ray(void);
+
+		Ray(const Point3D& origin, const Vector3D& dir);
+
+		Ray(const Ray& ray);
+
+		Ray&
+		operator= (const Ray& rhs);
+
+		~Ray(void);
+};
+
+#endif

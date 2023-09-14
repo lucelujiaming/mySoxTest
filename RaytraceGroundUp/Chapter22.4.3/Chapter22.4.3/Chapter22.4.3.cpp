481bb047d99ba578e19674bf8792e96756b9691b
xungao <xungao@xungao.com>
2023-09-14 16:06:05 +0800

    Add code in Chapter 18-10, 19, 21, 22

---
+++ RaytraceGroundUp/Chapter22.4.3/Chapter22.4.3/Chapter22.4.3.cpp
@@ -0,0 +1,26 @@
+// Chapter22.4.3.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#include "World.h"
+
+#include <fstream>
+#include <iostream>
+using namespace std;
+ofstream out;
+
+int
+main(void)
+{
+	out.open("fileppm.ppm", ios::out);
+	out << "P3\n"
+		<< 400 << " " << 400 << "\n255\n";
+	World w;
+	w.build();
+	// w.render_scene();
+	w.camera_ptr->render_scene(w);
+
+	out.close();
+	return(0);
+}

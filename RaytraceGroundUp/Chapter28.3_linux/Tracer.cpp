c5e9d96053ee8efa0c9ae83d4de7527bca5eaa4a
xungao <xungao@xungao.com>
2023-09-15 13:29:46 +0800

    Add code of Chapter 23 - 28

---
+++ RaytraceGroundUp/Chapter28.3_linux/Tracer.cpp
@@ -0,0 +1,48 @@
+#include "stdafx.h"
+#include "Tracer.h"
+
+// -------------------------------------------------------------------- default constructor
+
+Tracer::Tracer(void)
+	: world_ptr(NULL)
+{}
+
+
+// -------------------------------------------------------------------- constructor
+
+Tracer::Tracer(World* _worldPtr)
+	: world_ptr(_worldPtr)
+{}
+
+
+
+// -------------------------------------------------------------------- destructor
+
+Tracer::~Tracer(void) {
+	if (world_ptr)
+		world_ptr = NULL;
+}
+
+
+// -------------------------------------------------------------------- trace_ray
+
+RGBColor
+Tracer::trace_ray(const Ray& ray) const {
+	return (black);
+}
+
+
+// -------------------------------------------------------------------- trace_ray
+
+RGBColor
+Tracer::trace_ray(const Ray ray, const int depth) const {
+	return (black);
+}
+
+
+// -------------------------------------------------------------------- trace_ray
+
+RGBColor
+Tracer::trace_ray(const Ray ray, float& tmin, const int depth) const {
+	return (black);
+}

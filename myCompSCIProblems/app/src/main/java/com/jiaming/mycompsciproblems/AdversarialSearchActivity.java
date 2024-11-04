e4107e3c162595e4dcc1b85b0fb60f8493601784
xungao <xungao@xungao.com>
2024-11-04 17:29:25 +0800

    Finish KnapSack

--- myCompSCIProblems/app/src/main/java/com/jiaming/mycompsciproblems/AdversarialSearchActivity.java
+++ myCompSCIProblems/app/src/main/java/com/jiaming/mycompsciproblems/AdversarialSearchActivity.java
@@ -3,6 +3,13 @@ package com.jiaming.mycompsciproblems;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 public class AdversarialSearchActivity extends AppCompatActivity {
 
     @Override
@@ -56,6 +63,74 @@ public class AdversarialSearchActivity extends AppCompatActivity {
         TTTBoard testBoard3 = new TTTBoard(toWinHardPosition, TTTPiece.X);
         Integer answer3 = Minimax.findBestMove(testBoard3, 8);
         return assertEquality(answer3, 1);
+    }
+
+    public void testKnapSack() {
+        List<KnapSack.Item> items = new ArrayList<>();
+        items.add(new KnapSack.Item("candlesticks", 2, 300));
+        items.add(new KnapSack.Item("laptop", 3, 1000));
+        items.add(new KnapSack.Item("food", 1, 50));
+        items.add(new KnapSack.Item("painting", 2, 400));
+//        items.add(new Item("television", 50, 500));
+//        items.add(new Item("stereo", 35, 400));
+//        items.add(new Item("clothing", 20, 800));
+//        items.add(new Item("jewelry", 1, 4000));
+//        items.add(new Item("books", 100, 300));
+//        items.add(new Item("printer", 18, 30));
+//        items.add(new Item("refrigerator", 200, 700));
+        List<KnapSack.Item> toSteal = KnapSack.knapSack(items, 5);
+        System.out.println("The best items for the thief to steal are:");
+        System.out.printf("%-15.15s %-15.15s %-15.15s%n", "Name", "Weight", "Value");
+        for (KnapSack.Item item : toSteal) {
+            System.out.printf("%-15.15s %-15.15s %-15.15s%n", item.name, item.weight, item.value);
+        }
+    }
+
+    public void testDistances() {
+        Map<String, Map<String, Integer>> vtDistances = new HashMap<>();
+        Map<String, Integer> mapRutland = new HashMap<>();
+        mapRutland.put("Burlington", 67);
+        mapRutland.put("White River Junction", 46);
+        mapRutland.put("Bennington", 55);
+        mapRutland.put("Brattleboro", 75);
+        vtDistances.put("Rutland", mapRutland);
+
+        Map<String, Integer> mapWhiteRiverJunction = new HashMap<>();
+        mapWhiteRiverJunction.put("Rutland", 46);
+        mapWhiteRiverJunction.put("Burlington", 91);
+        mapWhiteRiverJunction.put("Bennington", 98);
+        mapWhiteRiverJunction.put("Brattleboro", 65);
+        vtDistances.put("White River Junction", mapWhiteRiverJunction);
+
+        Map<String, Integer> mapBennington = new HashMap<>();
+        mapBennington.put("Rutland", 55);
+        mapBennington.put("Burlington", 122);
+        mapBennington.put("White River Junction", 98);
+        mapBennington.put("Brattleboro", 40);
+        vtDistances.put("Bennington", mapWhiteRiverJunction);
+
+        Map<String, Integer> mapBrattleboro = new HashMap<>();
+        mapBrattleboro.put("Rutland", 75);
+        mapBrattleboro.put("Burlington", 153);
+        mapBrattleboro.put("White River Junction", 65);
+        mapBrattleboro.put("Bennington", 40);
+        vtDistances.put("Brattleboro", mapWhiteRiverJunction);
+
+        TSP tsp = new TSP(vtDistances);
+        String[] shortestPath = tsp.findShortestPath();
+        int distance = tsp.pathDistance(shortestPath);
+        System.out.println("The shortest path is " + Arrays.toString(shortestPath) + " in " +
+                distance + " miles.");
+    }
+
+    public void testPhoneNumberMnemonics() {
+        System.out.println("Enter a phone number:");
+        Scanner scanner = new Scanner(System.in);
+        String phoneNumber = scanner.nextLine();
+        scanner.close();
+        System.out.println("The possible mnemonics are:");
+        PhoneNumberMnemonics pnm = new PhoneNumberMnemonics(phoneNumber);
+        System.out.println(Arrays.toString(pnm.getMnemonics()));
     }
 
     public void runAllTests() {
--- myCompSCIProblems/app/src/main/java/com/jiaming/mycompsciproblems/KnapSack.java
+++ myCompSCIProblems/app/src/main/java/com/jiaming/mycompsciproblems/KnapSack.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class KnapSack {
+
     public static final class Item {
-        public final String name ;
+        public final String name;
         public final int weight;
         public final double value;
 
@@ -19,7 +20,15 @@ public final class KnapSack {
     public static List<Item> knapSack(List<Item> items, int maxCapacity) {
         // 创建一个表格，用于保存每种容量和物品数量组合的最优解。
         // 这个表格在填充的时候，是从1开始的。因此上，第0行和第0列始终都为全0。
+        // build up dynamic programming table
         double[][] table = new double[items.size() + 1][maxCapacity + 1];
+
+        System.out.println("The Items are:");
+        for (int i = 0; i < items.size(); i++) {
+            Item item = items.get(i);
+            System.out.printf("name: %-15s     Weight: %-15d     value: %-15f \n", 
+                    item.name, item.weight, item.value);
+        }
 
         // 遍历每种可能的物品数量，
         // 请注意，这里是“每种可能的物品数量”，而不是每一件物品。i等于2不代表第2件物品，
@@ -28,46 +37,90 @@ public final class KnapSack {
             // item是正要被窃取的下一件物品：
             Item item = items.get(i);
             // 对于每种可能的物品数量，我们都将线性遍历所有可能的重量，一直到背包的最大容纳重量。
-            for (int capacity = 0; capacity <= maxCapacity; capacity++) {
+            for (int capacity = 1; capacity <= maxCapacity; capacity++) {
 
                 // prevItemValue是正在探索的当前capacity（容量）以内最后一种物品组合的价值。
                 // 对于每种可能的物品组合，我们都要考虑是否还有可能加入“新”的物品。
-                double preItemValue = table[i][capacity];
+                double prevItemValue = table[i][capacity];
 
                 // 否则，我们就要考虑在当前容量以内，加入“新”物品能否产生比最后一种物品组合更高的价值。
-                if (capacity >= item.weight) {
+                if (capacity >= item.weight) { // item fits in knapsack
                     // 表中已算出的价值是指从当前容量中减去该物品重量后的容量对应的最近一次物品组合的价值。
-                    double valueFreeWeightForitem = table[i][capacity - item.weight];
+                    double valueFreeingWeightForItem = table[i][capacity - item.weight];
 
                     // 只要将该物品的价值加上表中已算出的价值即可得知，
-                    double doubleWeightWithCurrentItem = valueFreeWeightForitem + item.value;
+                    // only take if more valuable than previous item
+                    double doubleWeightWithCurrentItem = valueFreeingWeightForItem + item.value;
                     // 如果总价值高于当前容量下的最后一种物品组合的价值，就将其插入表中，
-                    if(doubleWeightWithCurrentItem > preItemValue) {
+                    // table[i + 1][capacity] = Math.max(valueFreeingWeightForItem + item.value, prevItemValue);
+                    if(doubleWeightWithCurrentItem > prevItemValue) {
                         table[i + 1][capacity] = doubleWeightWithCurrentItem;
                     }
                     // 否则插入最后一种组合的价值：
                     else {
-                        table[i + 1][capacity] = preItemValue;
+                        table[i + 1][capacity] = prevItemValue;
                     }
                 }
                 // 如果可以容纳的重量没有超过下一个物品的重量，说明可以把当前物品放入。
-                else {
-                    table[i + 1][capacity] = preItemValue;
+				else { // no room for this item
+                    table[i + 1][capacity] = prevItemValue;
                 }
             }
+
+            System.out.println("------------------------------------------------------------------------------------------------------------------------");
+            System.out.printf(
+            		"The current table's contents after put (name: %s, Weight: %d, value: %.1f)  are: \n", 
+                    item.name, item.weight, item.value);
+            System.out.print(
+                    "Check weight is   : ");
+            for(int k = 0; k <= maxCapacity; k++) {
+                System.out.printf("%d 磅     ", k );
+            }
+            System.out.println(" ");
+            for(int j = 0; j <= items.size(); j++) {
+                System.out.print(
+                        "可以放入"+ j +"个物体时: ");
+                for(int k = 0; k <= maxCapacity; k++) {
+                    System.out.printf("%-7.1f  ", table[j][k] );
+                }
+                System.out.println(" ");
+            }
+            
         }
+
+        System.out.println("------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("As the result, the table's contents are:");
+        System.out.print(
+                "Check weight is   : ");
+        for(int k = 0; k <= maxCapacity; k++) {
+            System.out.printf("%d 磅     ", k );
+        }
+        System.out.println(" ");
+        for(int j = 0; j <= items.size(); j++) {
+            System.out.print(
+                    "可以放入"+ j +"个物体时: ");
+            for(int k = 0; k <= maxCapacity; k++) {
+                System.out.printf("%-7.1f  ", table[j][k] );
+            }
+            System.out.println(" ");
+        }
+        
+        
 
         // 至此，建表的工作就完成了。但是，要想真正得到结果中的物品，
         // 需要从最高容量值及最终求得的物品组合开始往回找：
+        // figure out solution from table
         List<Item> solution = new ArrayList<>();
         int capacity = maxCapacity;
         // 我们从最终位置开始，从右到左遍历缓存表，
-        for (int i = items.size(); i> 0; i--) {
+        for (int i = items.size(); i > 0; i--) { // work backwards
             // 检查插入表中的总价值是否有变化。
-            if(table[i - 1][capacity] != table[i][capacity]) {
+            // was this item used?
+            if (table[i - 1][capacity] != table[i][capacity]) {
                 // 如果有，就意味着在计算某组合时加入了新的物品，因为该组合比前一组合价值高。
                 solution.add(items.get(i - 1));
                 // 于是我们把该物品加入解中。同时，要从总容量中减去该物品的重量，这可以被认为是在表中向上移动：
+                // if the item was used, remove its weight
                 capacity -= items.get(i - 1).weight;
             }
         }

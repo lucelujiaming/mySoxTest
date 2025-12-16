3fa069196a537e40fdf596e43171c3615d0beaf8
xungao <xungao@xungao.com>
2025-12-16 16:13:41 +0800

    Remove eth detection and use ptc to detect timeout

--- ptc310_app/Script/arping_daemon.sh
+++ ptc310_app/Script/arping_daemon.sh
@@ -3,8 +3,6 @@
 # cat sys_config.ini | grep "Service" | sed 's/ //g' | sed "s/.*=//"
 # To accelerate the startup process
 ETH_INIT_COUNT=10
-# We send $ETH_RECOVER_COUNT when the cable reconnect
-ETH_RECOVER_COUNT=10
 # $ARPING_SPAN is 10-6s, so 300000 is 0.3s
 # It is not acurate for the shell environment.
 ARPING_SPAN=300000
@@ -29,58 +27,26 @@ if [ $# -ge 2 ] ; then
         # sleep 2
         usleep $ARPING_SPAN
     done
-    # Starting cycle to check the eth0 and eth1
-    eth_status=0
     while true; do
-        eth0result=`mstpctl showtreeport br0 eth0 0 | grep "role" | awk -F" " '{print $2}'`
-        eth1result=`mstpctl showtreeport br0 eth1 0 | grep "role" | awk -F" " '{print $2}'`
-        if [ "$eth0result" == "Disabled" ]; then
-            # traceroute -i br0 192.168.10.12 > /dev/null
-            eth_status=$ETH_RECOVER_COUNT
-            /usr/sbin/arping -f -b -I br0 -s 192.168.10.$1 192.168.10.$2
-            echo "-------------------- eth0Disabled eth_status=$eth_status --------------------"
-            # sleep 2
-            usleep $ARPING_SPAN
-        elif [ "$eth1result" == "Disabled" ]; then
-            # traceroute -i br0 192.168.10.12 > /dev/null
-            eth_status=$ETH_RECOVER_COUNT
-            /usr/sbin/arping -f -b -I br0 -s 192.168.10.$1 192.168.10.$2
-            echo "-------------------- eth1Disabled eth_status=$eth_status --------------------"
-            # sleep 2
-            usleep $ARPING_SPAN
-        else
-            # eth0&eth1 Restore
-            if [ $eth_status -gt 0 ]; then
-                # traceroute -i br0 192.168.10.12 > /dev/null
+        ptcTimeout=`cat /root/app/sys_config.ini | grep "ModbusNetworkTimeout" | sed 's/ //g' | sed "s/.*=//"`
+        if [ "$ptcTimeout" == "1" ]; then
+            if [ $last_ptc_timeout -eq 0 ]; then
                 /usr/sbin/arping -f -b -I br0 -s 192.168.10.$1 192.168.10.$2
-                echo "-------------------- br0Restore eth_status=$eth_status --------------------"
-                if [ $? -eq 0 ]; then
-                    eth_status=$((eth_status - 1))
-                fi
-            # Heart beat
-            else
-                ptcTimeout=`cat /root/app/sys_config.ini | grep "ModbusNetworkTimeout" | sed 's/ //g' | sed "s/.*=//"`
-                if [ "$ptcTimeout" == "1" ]; then
-                    if [ $last_ptc_timeout -eq 0 ]; then
-                        /usr/sbin/arping -f -b -I br0 -s 192.168.10.$1 192.168.10.$2
-                        echo "-------------------- HeartBeat startup arping return=$? --------------------"
-                        last_ptc_timeout=1
-                    fi
-                    if [ $heartbeat_counter -gt $HEARTBEAT_TIMES ]; then
-                        /usr/sbin/arping -f -b -I br0 -s 192.168.10.$1 192.168.10.$2
-                        echo "-------------------- HeartBeat arping return=$? --------------------"
-                        heartbeat_counter=0
-                    fi
-                    heartbeat_counter=$((heartbeat_counter + 1))
-                else
-                    if [ $last_ptc_timeout -eq 1 ]; then
-                        last_ptc_timeout=0
-                    fi
-                fi
+                echo "-------------------- HeartBeat startup arping return=$? --------------------"
+                last_ptc_timeout=1
             fi
-            # sleep 2
-            usleep $ARPING_SPAN
+            if [ $heartbeat_counter -gt $HEARTBEAT_TIMES ]; then
+                /usr/sbin/arping -f -b -I br0 -s 192.168.10.$1 192.168.10.$2
+                echo "-------------------- HeartBeat arping return=$? --------------------"
+                heartbeat_counter=0
+            fi
+            heartbeat_counter=$((heartbeat_counter + 1))
+        else
+            if [ $last_ptc_timeout -eq 1 ]; then
+                last_ptc_timeout=0
+            fi
         fi
+        usleep $ARPING_SPAN
         # arp -d 192.168.10.$2
     done
 else
